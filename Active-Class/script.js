90d323622618f5e4dc3cd2dda394ab61e999cb56
ArsenTech <62609185+ArsenTech@users.noreply.github.com>
2025-07-10 00:06:26 +0400

    Removed script.js with either no content, and moved script.js with little content to html script tag

--- Active-Class/script.js
+++
@@ -1,8 +0,0 @@
-let nav = document.querySelector(".nav");
-let link = document.querySelectorAll(".link");
-link.forEach(function(a){
-     a.addEventListener("click", function(){
-          nav.querySelector(".active").classList.remove("active");
-          a.classList.add("active");
-     })
-})
